073e2a0efd978668ec6554059f330290056d1f60
Joyce Silva <joyce.silva@tucurui.ufpa.br>
2025-04-16 19:12:27 -0300

    Nomeação de rotas independente de mudanças no nome da view

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,21 @@ use Illuminate\Support\Facades\Route;
     return 'olá, bem vinde';
 });*/
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos' );
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato' );
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
+
+Route::prefix('app')->group(function() {
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+});
+
+/*
 Route::get('/contato/{nome}/{categoria_id}',
     function(
         string $nome = 'Desconhecido',
