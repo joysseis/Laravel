9c844b965fa2835026c8f7fead9ca76bb411cb0c
Joyce Silva <joyce.silva@tucurui.ufpa.br>
2025-04-21 20:49:07 -0300

    modificação de template para o arquivo basico no diretorio "site/layouts", usando @extends @section, @yields

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Contato</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{asset('css/estilo_basico.css')}}">
-        
-    </head>
+@extends('site.layouts.basico')
 
-    <body>
-        <div class="topo">
+@section('conteudo')
+ <div class="topo">
 
             <div class="logo">
                 <img src="img/logo.png">
@@ -70,5 +63,4 @@
                 <img src="{{asset('img/mapa.png')}}">
             </div>
         </div>
-    </body>
-</html>
+@endsection
