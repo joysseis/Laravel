2495e16c053b4068f00d494253a94ed3fe0baac4
Joyce Silva <joyce.silva@tucurui.ufpa.br>
2025-04-23 21:18:06 -0300

    enviando paramtetros para @components usando a variavel $slot uma variavel que funciona de forma similar ao @yield()

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,7 +11,9 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    @component('site.layouts._components.form_contato')
+                    @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                        <p> A nossa equipe analisará a sua mensagem e retornaremos</p>
+                        <p>Nosso tempo de resposta é 24 horas.</p>
                     @endcomponent
                 </div>
             </div>
