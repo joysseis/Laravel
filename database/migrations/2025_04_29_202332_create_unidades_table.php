bcaa9d09750b13ca41dbdb76645615291e7a8a4a
Joyce Silva <joyce.silva@tucurui.ufpa.br>
2025-04-30 22:09:01 -0300

    adicionando chave estrangeira um para outro

--- database/migrations/2025_04_29_202332_create_unidades_table.php
+++ database/migrations/2025_04_29_202332_create_unidades_table.php
@@ -17,6 +17,22 @@ class CreateUnidadesTable extends Migration
             $table->id();
             $table->timestamps();
         });
+
+        //adicionar o relacionamento com a tabela produtos
+        Schema::create('unidade', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->reference('id')->on('unidades');
+        });
+        //adicionar o relacionamento com a tabela produtos
+        Schema::table('produtos', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->reference('id')->on('unidades');
+        });
+        //adicionar o relacionamento com a tabela produtos_detalhes
+        Schema::table('produtos_detalhes', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->reference('id')->on('unidades');
+        });
     }
 
     /**
@@ -26,6 +42,13 @@ class CreateUnidadesTable extends Migration
      */
     public function down()
     {
+        //remover o relacionamento com a tabela produto_detalhes
+        Schema::table('produtos_detalhes', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->reference('id')->on('unidades');
+        });
+
+        //remover o relacionamento com a tabela produto
         Schema::dropIfExists('unidades');
     }
 }
