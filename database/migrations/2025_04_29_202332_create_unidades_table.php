140e5430aa295134b9d2fe2c9d5fab0cbc83a20b
Joyce Silva <joyce.silva@tucurui.ufpa.br>
2025-05-01 21:54:50 -0300

    relacionamento um para varios

--- database/migrations/2025_04_29_202332_create_unidades_table.php
+++ database/migrations/2025_04_29_202332_create_unidades_table.php
@@ -15,23 +15,21 @@ class CreateUnidadesTable extends Migration
     {
         Schema::create('unidades', function (Blueprint $table) {
             $table->id();
+            $table->string('unidade', 5); //cm, mn, kg
+            $table->string('descricao', 30);
             $table->timestamps();
         });
 
         //adicionar o relacionamento com a tabela produtos
-        Schema::create('unidade', function(Blueprint $table){
+        Schema::table('produtos', function(Blueprint $table) {
             $table->unsignedBigInteger('unidade_id');
-            $table->foreign('unidade_id')->reference('id')->on('unidades');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
         });
-        //adicionar o relacionamento com a tabela produtos
-        Schema::table('produtos', function(Blueprint $table){
+
+        //adicionar o relacionamento com a tabela produto_detalhes
+        Schema::table('produto_detalhes', function(Blueprint $table) {
             $table->unsignedBigInteger('unidade_id');
-            $table->foreign('unidade_id')->reference('id')->on('unidades');
-        });
-        //adicionar o relacionamento com a tabela produtos_detalhes
-        Schema::table('produtos_detalhes', function(Blueprint $table){
-            $table->unsignedBigInteger('unidade_id');
-            $table->foreign('unidade_id')->reference('id')->on('unidades');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
         });
     }
 
@@ -43,12 +41,21 @@ class CreateUnidadesTable extends Migration
     public function down()
     {
         //remover o relacionamento com a tabela produto_detalhes
-        Schema::table('produtos_detalhes', function(Blueprint $table){
-            $table->unsignedBigInteger('unidade_id');
-            $table->foreign('unidade_id')->reference('id')->on('unidades');
+        Schema::table('produto_detalhes', function(Blueprint $table) {
+            //remover a fk
+            $table->dropForeign('produto_detalhes_unidade_id_foreign'); //[table]_[coluna]_foreign
+            //remover a coluna unidade_id
+            $table->dropColumn('unidade_id');
         });
 
-        //remover o relacionamento com a tabela produto
+        //remover o relacionamento com a tabela produtos
+        Schema::table('produtos', function(Blueprint $table) {
+            //remover a fk
+            $table->dropForeign('produtos_unidade_id_foreign'); //[table]_[coluna]_foreign
+            //remover a coluna unidade_id
+            $table->dropColumn('unidade_id');
+        });
+
         Schema::dropIfExists('unidades');
     }
 }
